adf2a195a6e8f32b0992df23b13777884bbc5315
mhmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-05-20 23:51:57 +0200

    add lab3

---
+++ day3/blogApp/app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController  
+  # before_action :save_login_state, :only => [:new, :create]
+ 
+end
---
+++ day3/blogApp/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+
+  devise_for :users
+  root 'welcome#index'
+
+  
+  resources :articles do
+    resources :comments
+  end
+
+
+
+
+
+end
